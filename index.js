6cca4d29eeb0d6f2346fa100e24fd45e54ddbcb7
pmqueiroz <pedroqueiroz593@gmail.com>
2021-05-21 14:49:22 -0300

    feat: hour mask

--- index.js
+++ index.js
@@ -29,3 +29,35 @@ const time = (timeInput, maxLength) => {
    }
 }
 exports.time = time
+
+const hour = (hourInput, complete, symbol  = ':') => {
+const regex = /(\d{2})(\d{2})/
+   const trimmedHour = hourInput.replace(/\D/g, '')
+
+   if (complete && trimmedHour.length < 4) {
+      switch (trimmedHour.length) {
+         case 1:
+         let hour_1 = `${trimmedHour}0`
+         if (Number(hour_1) > 9) hour_1 = `0${trimmedHour}`
+         const minute_1 = '00'
+         return `${Number(hour_1) > 23 ? '23' : hour_1}${symbol}${minute_1}`
+         case 2:
+         const minute_2 = '00'
+         return `${Number(trimmedHour) > 23 ? '23' : trimmedHour}${symbol}${minute_2}`
+         case 3:
+         const regex = /(\d{2})(\d{1})/
+         const hour_3 = trimmedHour.replace(regex, '$1')
+         const minute_3 = `${trimmedHour.replace(regex, '$2')}0`
+         return `${Number(hour_3) > 23 ? '23' : hour_3}${symbol}${Number(minute_3) > 59 ? '59' : minute_3}`
+      }
+   }
+
+   if (trimmedHour.length < 4) return trimmedHour
+
+   const hour = trimmedHour.replace(regex, '$1')
+   const minute = trimmedHour.replace(regex, '$2')
+
+   return `${Number(hour) > 23 ? '23' : hour}${symbol}${Number(minute) > 59 ? '59' : minute}`
+}
+
+exports.hour = hour
--- index.spec.js
+++ index.spec.js
@@ -1,13 +1,45 @@
-const { time } = require('./index')
+const maskWizard = require('./index')
+
+
 describe('time', () => {
    it('is a function', function() {
-      expect(typeof time).toBe('function')
+      expect(typeof maskWizard.time).toBe('function')
    })
 
    it('mask time input', () => {
-      expect(time(1)).toBe('0h01m')
-      expect(time(13)).toBe('0h13m')
-      expect(time(131)).toBe('1h31m')
-      expect(time(1312)).toBe('13h12m')
+      expect(maskWizard.time(1)).toBe('0h01m')
+      expect(maskWizard.time(13)).toBe('0h13m')
+      expect(maskWizard.time(131)).toBe('1h31m')
+      expect(maskWizard.time(1312)).toBe('13h12m')
+   })
+})
+
+describe('hour', () => {
+   it('is a function', function() {
+      expect(typeof maskWizard.hour).toBe('function')
+   })
+
+   it('mask hour input', () => {
+      const hour = '1155'
+      const incompleteHour1 = '1'
+      const incompleteHour2 = '11'
+      const incompleteHour3 = '115'
+      const wrongHour = '2555'
+      const wrongMinutes = '1160'
+      const incompleteWrongHour = '25'
+      const incompleteWrongMinutes = '116'
+
+      expect(maskWizard.hour(hour)).toEqual('11:55')
+      expect(maskWizard.hour(wrongHour)).toEqual('23:55')
+      expect(maskWizard.hour(wrongMinutes)).toEqual('11:59')
+      expect(maskWizard.hour(incompleteHour1, true)).toEqual('01:00')
+      expect(maskWizard.hour(incompleteHour2, true)).toEqual('11:00')
+      expect(maskWizard.hour(incompleteHour3, true)).toEqual('11:50')
+      expect(maskWizard.hour(incompleteWrongHour, true)).toEqual('23:00')
+      expect(maskWizard.hour(incompleteWrongMinutes, true)).toEqual('11:59')
+   })
+
+   it('mask hour input with a different symbol', () => {
+      expect(maskWizard.hour('1155', null , '?')).toEqual('11?55')
    })
 })
