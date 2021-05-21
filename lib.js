6fd9ae3763f1417b85558a6c641f8786fd61fda3
pmqueiroz <pedroqueiroz593@gmail.com>
2021-05-21 10:38:13 -0300

    refactor: remove leading file

--- lib.js
+++
@@ -1,7 +0,0 @@
-const onlyNumbers = (str) => String(str).replace(/\D/g, '')
-
-const noLeadingZero = (nmb) => String(nmb).replace(/^0+(?!$)/, '')
-
-module.exports.onlyNumbers = onlyNumbers
-
-module.exports.noLeadingZero = noLeadingZero
