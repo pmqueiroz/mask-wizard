edf75e72a6ec5fb850eacf0c1c7acabaa7a6d1db
Pedro Queiroz <pedroqueiroz593@gmail.com>
2021-10-07 23:36:21 -0300

    chore: tests workflows (#17)
    * chore: better setup for jest
    
    * chore: workflow in every pull request and push

--- __tests__/time/hour.spec.ts
+++ __tests__/time/hour.spec.ts
@@ -1,18 +1,4 @@
-const maskWizard = require('./dist')
-
-
-describe('interval', () => {
-   it('is a function', function() {
-      expect(typeof maskWizard.time.interval).toBe('function')
-   })
-
-   it('mask interval input', () => {
-      expect(maskWizard.time.interval(1)).toBe('0h01m')
-      expect(maskWizard.time.interval(13)).toBe('0h13m')
-      expect(maskWizard.time.interval(131)).toBe('1h31m')
-      expect(maskWizard.time.interval(1312)).toBe('13h12m')
-   })
-})
+import * as maskWizard from '../../dist'
 
 describe('hour', () => {
    it('is a function', function() {
