cf2fe5ef52903c629059fa523461121cf7643810
Ana Carolina <ana_carolina_88@live.com>
2021-10-09 18:30:02 -0300

    feat: adding main page

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -3,15 +3,42 @@ import clsx from 'clsx'
 import Layout from '@theme/Layout'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import styles from './index.module.css'
+import logoHero from '../../static/img/logo-hero.png'
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext()
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        Salve Brasileirinho
+     <div className={clsx('first--section', styles.container)}>
+       <div className={clsx('hero hero--primary', styles.decorationOne)}/>
+       <div className={clsx('hero hero--primary', styles.decorationTwo)}/>
+       <div className={clsx('hero hero--primary', styles.decorationThree)}/>
+       <div className={clsx('hero hero--primary', styles.decorationFour)}/>
+       <div className={clsx('hero hero--primary', styles.decorationFive)}/>
+
+        <div className={clsx('second--section', styles.contentTexts)}>
+          <h1 className={clsx('hero hero--primary', styles.firstText)}>Mask Wizard</h1>
+          <p className={clsx('hero hero--primary', styles.secondText)}>Simple usage string masking functions, checkout our documentation.</p>
+          <a href="/docs/Intro"><button className={clsx('hero hero--primary',styles.buttons)}>Get start</button></a>
+        </div>
+
+        <div className={clsx('hero hero--primary', styles.imgContainer)}>
+          <img src={logoHero}/>
+        </div>                   
       </div>
     </header>
+  )
+}
+
+function HomepageBody() {
+  const {siteConfig} = useDocusaurusContext()
+  return ( 
+    <section className={clsx('firstSection', styles.firstSection)}>
+    <div className="container">
+       <p className={clsx('firstSection', styles.firstBodyText)}>Simple usage string <span className={clsx('firstSection', styles.decorateBodyText)}>masking functions</span>, checkout our documentation.</p>
+       <p className={clsx('firstSection', styles.secondBodyText)}>Pellentesque sit amet facilisis nulla, nam tincidunt urna quis efficitur cursus. </p>
+       </div>
+</section>
   )
 }
 
@@ -22,6 +49,7 @@ export default function Home() {
       title={`${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
+      <HomepageBody/>
     </Layout>
   )
 }
